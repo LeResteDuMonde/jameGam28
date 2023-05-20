93973191a9d900645ee292c195c73b16f18d9492
Jules Pesin <jules.pesin@gmail.com>
2023-05-20 20:18:43 +0200

    fixed bomb?

--- script/bomb.gd
+++ script/bomb.gd
@@ -23,13 +23,6 @@ func explode():
 	
 	var nodes = impact.get_overlapping_bodies()
 	for n in nodes:
-#		print(n)
 		if(n.name == "Enemy"): n.queue_free()
 		
 	queue_free()
-	
-
-
-
-#func _on_impact_area_shape_entered(area_rid, area, area_shape_index, local_shape_index):
-#	pass # Replace with function body.
